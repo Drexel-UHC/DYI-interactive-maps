243ecc439ceb23857eff0ef0a4cb560c767ddf67
rl627 <rl627@drexel.edu>
2021-08-31 10:38:42 -0400

    input data format v2

--- Data/salurbal_data_sim.R
+++ Data/salurbal_data_sim.R
@@ -5,31 +5,16 @@ set.seed(123)
 # Load Shape Files
 load("../../SALURBAL COVID-19 Dashboard/Data/salurbal_l1_sf_simp.rdata")
 load("../../SALURBAL COVID-19 Dashboard/Data/sf_salurbal_0.8.rdata")
-l1_keys = salurbal_l1_sf_simp %>% 
-  mutate(key = stri_rand_strings(nrow(salurbal_l1_sf_simp), 10, pattern = "[A-Za-z0-9]")) %>% 
+sf_l1_simp = salurbal_l1_sf_simp %>% 
+  select(salid1, country, name)
+sf_l1_hex = sf_salurbal_0.8%>% 
+  select(salid1, country, name)
+save(sf_l1_simp,sf_l1_hex,
+     file = "../App/sf_files.rdata")
+
+bogus_l1_data = sf_l1_simp %>% 
   as_data_frame() %>% 
-  select(salid1,key)
-
-shp_bogus_l1 = salurbal_l1_sf_simp %>% 
-  left_join(l1_keys) %>% 
-  select(key,country, name)
-st_write(shp_bogus_l1, "l1.shp",  delete_layer  = T)
-
-# Load Hex
-hex_bogus_l1 = sf_salurbal_0.8 %>% 
-  left_join(l1_keys %>% select(key, salid1)) %>% 
-  select(key, country, name)
-st_write(hex_bogus_l1, "hex_l1.shp",  delete_layer  = T)
-
-## Save as rdta
-fileSfL1 = shp_bogus_l1
-fileSfL1Hex = hex_bogus_l1
-save(fileSfL1,fileSfL1Hex, file = "../App/default_shp.rdata")
-
-## Simulate burger data
-bogus_l1_data = shp_bogus_l1 %>% as_data_frame() %>% 
   select(-geometry)  %>% 
-  left_join(bogus_l1_means) %>% 
   group_by(country) %>% 
   group_modify(~{
     L1_count = .x %>% nrow()
@@ -38,6 +23,6 @@ bogus_l1_data = shp_bogus_l1 %>% as_data_frame() %>%
              var_catsPer100k =  rnorm(L1_count,rnorm(1, 400,25), rnorm(1,20,2)))
   }) %>% 
   ungroup() %>% 
-  select(-mean)
-write.csv(bogus_l1_data,file = "bogus_l1_data.csv")
+  select(-country, -name)
+write.csv(bogus_l1_data,file = "bogus_l1_data_v2.csv")
 
